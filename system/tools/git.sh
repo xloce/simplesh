6430934b546c4e980b30c42777bfbdcedf3e1a77
Cole Bishop <colewadebishop@gmail.com>
2019-05-07 01:34:12 -0500

    Redid git CLI tool installation.

--- system/tools/git.sh
+++ system/tools/git.sh
@@ -1,17 +1,29 @@
-# diff-so-fancy
-if ! which diff-so-fancy > /dev/null; then 
-	arr[0]="https://github.com/paulirish/git-recent/blob/master/git-recent"
-	arr[1]="https://raw.githubusercontent.com/so-fancy/diff-so-fancy/master/third_party/build_fatpack/diff-so-fancy"
+#!/bin/bash
 
-	mkdir -p "$HOME/.local/bin"
-	for gitTool in ${arr[*]}
-	do
-		 (cd "$HOME/.local/bin" && \
-		 curl -Os  && "$gitTool" \
-		 chmod +x diff-so-fancy)
-	done
-fi
-if which pip > /dev/null; then # https://github.com/tkrajina/git-plus
-	pip install git-plus;
-	pip install git-playback;
-fi
+# list of shell scrip installed git tools
+tools=( "https://raw.githubusercontent.com/paulirish/git-recent/master/git-recent" \
+"https://raw.githubusercontent.com/so-fancy/diff-so-fancy/master/third_party/build_fatpack/diff-so-fancy" \
+"https://raw.githubusercontent.com/davidosomething/git-my/master/git-my" \
+"https://raw.githubusercontent.com/davidosomething/git-ink/master/git-ink");
+
+# list of pip installed tools
+pips=( git-plus git-playback );
+
+mkdir -p "$HOME/.local/bin";
+for gitTool in ${tools[*]}
+do
+	toolName=$(echo "$gitTool"|grep -oP '[^\/]+$')
+	if ! which "$toolName" > /dev/null; then
+		(cd "$HOME/.local/bin" && \
+		curl -Os "$gitTool" && \
+		chmod +x "$toolName" &&
+		echo "Installed $toolName")
+	fi
+done
+
+for pipGitTool in ${pips[*]}
+do
+	if ! pip list installed --format=legacy|grep "$pipGitTool" > /dev/null; then
+		pip install "$pipGitTool";
+	fi
+done
