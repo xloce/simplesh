0d43ba17deb428cbd8f0b6276ba9b3c9a407cd80
Cole Bishop <colewadebishop@gmail.com>
2019-05-07 00:42:06 -0500

    Added git CLI tools.

--- system/tools/git.sh
+++ system/tools/git.sh
@@ -1,7 +1,17 @@
 # diff-so-fancy
-if ! which diff-so-fancy > /dev/null; then # see: https://github.com/so-fancy/diff-so-fancy
-	 mkdir -p "$HOME/.local/bin"
-	 (cd "$HOME/.local/bin" && \
-	 curl -Os https://raw.githubusercontent.com/so-fancy/diff-so-fancy/master/third_party/build_fatpack/diff-so-fancy && \
-	 chmod +x diff-so-fancy)
+if ! which diff-so-fancy > /dev/null; then 
+	arr[0]="https://github.com/paulirish/git-recent/blob/master/git-recent"
+	arr[1]="https://raw.githubusercontent.com/so-fancy/diff-so-fancy/master/third_party/build_fatpack/diff-so-fancy"
+
+	mkdir -p "$HOME/.local/bin"
+	for gitTool in ${arr[*]}
+	do
+		 (cd "$HOME/.local/bin" && \
+		 curl -Os  && "$gitTool" \
+		 chmod +x diff-so-fancy)
+	done
+fi
+if which pip > /dev/null; then # https://github.com/tkrajina/git-plus
+	pip install git-plus;
+	pip install git-playback;
 fi
